1b18c6669da3e1f6d3419f774eaa5e5f4f96a49b
xw80329 <xliangwu@gmail.com>
2022-10-05 19:30:16 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,39 @@
+const getConfig = require("vuepress-bar");
+const { nav, sidebar } = getConfig();
+
+module.exports = {
+    title: '代码驱动科技',
+    description: '编程知识体系梳理和总结',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ],
+    base: '/',
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        logo: '/images/code.png',
+        sidebarDepth: 2,
+        plugins: [
+            ['autobar', { 'pinyinNav': true, "maxLevel": 2 }],
+            ['permalink-pinyin'],
+            ["rpurl"],
+            ['@vuepress/back-to-top'],
+            ['@vuepress/search', {
+                searchMaxSuggestions: 10
+            }],
+        ],
+        nav: [
+            { text: '主页', link: '/' },
+            ...nav,
+            { text: '博客', link: 'http://obkoro1.com/' },
+            {
+                text: 'GitHub',
+                items: [
+                    { text: 'GitHub', link: 'https://github.com/xliangwu/coder_km' }
+                ]
+            },
+        ],
+        sidebar
+    }
+};
