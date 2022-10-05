28c05808c28f7becefc692183cfb85c438d680cd
xw80329 <xliangwu@gmail.com>
2022-10-05 20:18:52 +0800

    update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "${date} [INFO] sync data from github"
+echo "$(date) [INFO] sync data from github"
 git pull
 
-echo "${date} [INFO] vue buiild static web sites"
+echo "$(date) [INFO] vue buiild static web sites"
 yarn docs:build
 
 cp -R /home/xw80329/apps/coder_km/docs/.vuepress/dist/* /home/xw80329/coder_km
-echo "${date} [INFO] sync static web sites"
+echo "$(date) [INFO] sync static web sites"
