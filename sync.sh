83f63f44516ba50214efcbe4a9625682228faf06
xw80329 <xliangwu@gmail.com>
2022-10-07 22:05:39 +0800

    update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,8 +3,10 @@ cd /home/xw80329/apps/coder_km
 echo "$(date) [INFO] sync data from github"
 timeout 60 git pull
 
-echo "$(date) [INFO] vue buiild static web sites"
-yarn docs:build
+echo "$(date) [INFO] start to buiild static web sites"
+yarn docs:build &
+wait
+echo "$(date) [INFO] finish to buiild static web sites"
 
 cp -R /home/xw80329/apps/coder_km/docs/.vuepress/dist/* /home/xw80329/coder_km
 echo "$(date) [INFO] sync static web sites"
