c0383049640b2497a869880eb96a5ce8588edba1
xw80329 <xliangwu@gmail.com>
2022-11-25 21:34:22 +0800

    更新备案号

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,7 +1,8 @@
 <template>
     <ParentLayout>
         <template #page-bottom>
-            <div> Apache License 2.0 | Copyright © 2022 by xueliang.wu <a href="https://beian.miit.gov.cn/" target="_blank">苏ICP备15016087号</a>
+            <div style="text-align:center;padding:40px 0px"> Apache License 2.0 | Copyright © 2022 by xueliang.wu <a
+                    href="https://beian.miit.gov.cn/" target="_blank">苏ICP备15016087号</a>
             </div>
         </template>
     </ParentLayout>
--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,9 @@
 ---
 home: true
+heroText: 代码驱动科技
+tagline: 编程知识体系梳理和总结
+actionText: 快速上手 →
+actionLink: /nav.01.Linux知识/
 ---
 
 ::: tip
@@ -11,4 +15,9 @@ home: true
 - 博客主要是记录工作中用到的一些知识，平时用什么查什么，没有形成一个系统的记录，现在通过博客让自己形成良好的记录习惯。
 - 如果这些内容能帮助到大家或者解决一些小问题，那么我的内心也是喜悦的。
 - 博客内容正在慢慢书写和整理中，请耐心等待！
+:::
+
+
+::: slot footer
+Apache License 2.0 | Copyright © 2022 by xueliang.wu [苏ICP备15016087号](https://beian.miit.gov.cn)
 :::
